f6308fca90a3d332b88bfa93d9f04d87644ea710
mingmingtasd <mingming1.xu@intel.com>
2022-04-22 10:53:50 +0800

    Change ReturnIfFailed to ThrowIfFailed in Python\device.cpp

--- Python/src/device.cpp
+++ Python/src/device.cpp
@@ -16,7 +16,7 @@ using Microsoft::WRL::ComPtr;
 HRESULT IsWarpAdapter(IDXGIAdapter1* pAdapter, bool* isWarpAdapter)
 {
     DXGI_ADAPTER_DESC1 pDesc;
-    ReturnIfFailed(pAdapter->GetDesc1(&pDesc));
+    ThrowIfFailed(pAdapter->GetDesc1(&pDesc));
     // see here for documentation on filtering WARP adapter:
     // https://docs.microsoft.com/en-us/windows/desktop/direct3ddxgi/d3d10-graphics-programming-guide-dxgi#new-info-about-enumerating-adapters-for-windows-8
     auto isBasicRenderDriverVendorId = pDesc.VendorId == 0x1414;
@@ -46,12 +46,12 @@ Device::Device(bool useGpu, bool useDebugLayer, DXGI_GPU_PREFERENCE gpuPreferenc
     if (m_useGpu)
     {
         ComPtr<IDXGIFactory6> spFactory;
-        ReturnIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&spFactory)));
+        ThrowIfFailed(CreateDXGIFactory1(IID_PPV_ARGS(&spFactory)));
         UINT i = 0;
         while (spFactory->EnumAdapterByGpuPreference(i, m_gpuPreference, IID_PPV_ARGS(&dxgiAdapter)) != DXGI_ERROR_NOT_FOUND)
         {
             bool isWarpAdapter = false;
-            ReturnIfFailed(IsWarpAdapter(dxgiAdapter.Get(), &isWarpAdapter));
+            ThrowIfFailed(IsWarpAdapter(dxgiAdapter.Get(), &isWarpAdapter));
             if (!isWarpAdapter)
             {
                 break;
